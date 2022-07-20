f58b53dcd85ee189004a921c1c91dec898a5f195
Виталий Филонов <mutafrukt@MacBook-Air-Vitalij.local>
2022-07-20 15:47:33 +0300

    2

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -2,39 +2,44 @@ package ru.netology.sqr;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class SQRServiceTest {
 
-    @Test
-    void test1() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/sqr.csv")
+    void test1(int expected, int from, int to) {
+
         SQRService service = new SQRService();
 
-        int expected = 3;
-        int actual = service.countSqr(200, 300);
+        ///int expected = 3;
+        int actual = service.countSqr(from, to);
 
         Assertions.assertEquals(expected, actual);
 
     }
 
-    @Test
-    void test2() {
+///    @Test
+///    void test2() {
+///
+///        SQRService service = new SQRService();
+///        int expected = 14;
+///        int actual = service.countSqr(120, 600);
+///
+///     Assertions.assertEquals(expected, actual);
+///}
 
-        SQRService service = new SQRService();
-        int expected = 14;
-        int actual = service.countSqr(120, 600);
+///    @Test
+///    void test3() {
 
-        Assertions.assertEquals(expected, actual);
-    }
+///         SQRService service = new SQRService();
+///         int expected = 34;
+///         int actual = service.countSqr(18, 1900);
 
-    @Test
-    void test3() {
-
-        SQRService service = new SQRService();
-        int expected = 34;
-        int actual = service.countSqr(18, 1900);
-
-        Assertions.assertEquals(expected, actual);
-    }
+///     Assertions.assertEquals(expected, actual);
+/// }
 
 
 }
